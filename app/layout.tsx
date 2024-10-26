ac7c76d9687d22e35a9e1228fdc74887c1e3d3cd
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2024-10-26 15:20:23 +0100

    chore: Update layout.tsx to use light theme and add ToastProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,8 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider defaultTheme="light">
           <AuthProvider>{children}</AuthProvider>
+          <ToastProvider />
         </ThemeProvider>
       </body>
     </html>
